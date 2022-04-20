d668ea90abbd69971bd7d204361b3f0160d3f77a
chenhaijiang <chj@onyx-international.com>
2022-04-20 21:57:44 +0800

    sample migrate to androidx

--- projects/sample/source/sample-plugin/sample-base-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/gallery/cases/UseCaseSummaryFragment.java
+++ projects/sample/source/sample-plugin/sample-base-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/gallery/cases/UseCaseSummaryFragment.java
@@ -21,7 +21,7 @@ package com.tencent.shadow.sample.plugin.app.lib.gallery.cases;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
